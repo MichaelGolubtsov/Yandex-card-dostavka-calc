c1e67ffe00b6b79906d9bc9c13f99675fb2e59f9
mik174 <mik174@r608.ru>
2020-05-16 18:09:16 +0300

    Работает поиск маршрута и расчет его длины

---
+++ App/js/main.js
@@ -0,0 +1,113 @@
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [55.73, 37.75],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        moscowPolygon;
+
+    function onPolygonLoad (json) {
+        moscowPolygon = new ymaps.Polygon(json.coordinates);
+        // Если мы не хотим, чтобы контур был виден, зададим соответствующую опцию.
+        moscowPolygon.options.set({fillColor: "ffffff00",strokeWidth: 2 ,strokeColor: "ff0000aa" });
+        // Чтобы корректно осуществлялись геометрические операции
+        // над спроецированным многоугольником, его нужно добавить на карту.
+        myMap.geoObjects.add(moscowPolygon);
+
+        console.log('Poligon created');
+
+        ymaps.route(['Москва', 'Головково']).then(
+            function (res) {
+                // Объединим в выборку все сегменты маршрута.
+                var pathsObjects = ymaps.geoQuery(res.getPaths()),
+                    edges = [];
+
+                var PathLength=0;
+
+
+                var PathLength=7.4;
+                console.log('Length',PathLength);
+
+                // Переберем все сегменты и разобьем их на отрезки.
+                pathsObjects.each(function (path) {
+                    var coordinates = path.geometry.getCoordinates();
+                    for (var i = 1, l = coordinates.length; i < l; i++) {
+                        edges.push({
+                            type: 'LineString',
+                            coordinates: [coordinates[i], coordinates[i - 1]]
+                        });
+                    }
+                });
+
+                // Создадим новую выборку, содержащую:
+                // - отрезки, описываюшие маршрут;
+                // - начальную и конечную точки;
+                // - промежуточные точки.
+                var routeObjects = ymaps.geoQuery(edges)
+                        .add(res.getWayPoints())
+                        .add(res.getViaPoints())
+                        .setOptions('strokeWidth', 3)
+                        .addToMap(myMap),
+                    // Найдем все объекты, попадающие внутрь МКАД.
+                    objectsInMoscow = routeObjects.searchInside(moscowPolygon),
+                    // Найдем объекты, пересекающие МКАД.
+                    boundaryObjects = routeObjects.searchIntersect(moscowPolygon);
+
+                // Раскрасим в разные цвета объекты внутри, снаружи и пересекающие МКАД.
+                boundaryObjects.setOptions({
+                    strokeColor: '#06ff00',
+                    //preset: 'islands#greenIcon'
+
+                });
+
+                objectsInMoscow.setOptions({
+                    strokeColor: '#ffffff',
+                    visible: false
+                    //preset: 'islands#redIcon'
+                });
+
+                // Объекты за пределами МКАД получим исключением полученных выборок из
+                // исходной.
+                routeObjects.remove(objectsInMoscow).remove(boundaryObjects).setOptions({
+                    strokeColor: '#0010ff',
+                    preset: 'islands#blueIcon'
+                });
+
+                var MKADDistance = 0;
+                var insideMKADDistance = 0;
+//объекты "минус" то, что внутри
+//a-b
+                routeObjects.remove(objectsInMoscow).remove(boundaryObjects)
+                    .each(function (segment, i)
+                    {
+                        //есть ли такая фунция
+                        if (segment.geometry.getDistance != undefined)
+                            MKADDistance += segment.geometry.getDistance();
+                    });
+
+//то, что внутри
+//это как a-(a-b)=b
+                routeObjects.remove(routeObjects.remove(objectsInMoscow).remove(boundaryObjects))
+                    .each(function (segment, i)
+                    {
+                        if (segment.geometry.getDistance != undefined)
+                            insideMKADDistance += segment.geometry.getDistance();
+                    });
+
+                console.log('insideMKADDistance',insideMKADDistance);
+
+                console.log('MKADDistance',MKADDistance);
+
+            }
+        );
+    }
+
+    $.ajax({
+        url: 'moscow.json',
+        dataType: 'json',
+        success: onPolygonLoad
+    });
+}
